6faef7cc5a14536f8b33fac18faa0c661e8642c7
sammieneutron <samson.s.samuels@gmail.com>
2022-01-17 03:19:31 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import firebase from './FirebaseConfig'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Firebase Recipe</h1>
+    </div>
+  );
+}
+
+export default App;
